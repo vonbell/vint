64a98d084b80757dd2612744f704215b6500eafb
vonbell <divonvbell@gmail.com>
2022-05-18 08:51:53 -0400

    code cleanup

--- main_app/views.py
+++ main_app/views.py
@@ -1,33 +1,11 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
-from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth import login
 from .models import Vinyl, Photo
-from .forms import VinylForm
 import boto3
 import uuid
 
 S3_BASE_URL = 'https://s3.us-east-1.amazonaws.com/'
 BUCKET = 'vint'
-
-
-# def signup(request):
-#     error_message = ''
-#     if request.method == 'POST':
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)
-#             return redirect('index')
-#         else:
-#             error_message = 'invalid credentails - please try again'
-#     form = UserCreationForm()
-#     context = { 'form': form, 'error_message': error_message }
-#     return render(request, 'registration/signup.html', context)
 
 def home(request):
     return render(request, 'home.html')
